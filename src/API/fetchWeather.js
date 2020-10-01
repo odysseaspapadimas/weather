3d088b43d9851502b12055c562090d92e99c40c7
unknown <odysseas.patra@gmail.com>
2020-10-01 23:04:08 +0300

    Hid API key

--- src/API/fetchWeather.js
+++ src/API/fetchWeather.js
@@ -2,14 +2,14 @@ import axios from "axios";
 
 const API = {
   URL: "https://api.openweathermap.org/data/2.5/weather",
-  KEY: "d4e3c763024b362f191a09174b66ebde",
+  KEY: process.env.REACT_APP_API_KEY,
 };
 
 export const fetchWeather = async (query) => {
   try {
     const { data } = await axios(
       //`${API.URL}?q=${query}&exclude=daily,minutely,alerts&units=metric&appid=${API.KEY}`
-      `${API.URL}?q=${query}&exclude=daily,minutely,alerts&units=metric&appid=d4e3c763024b362f191a09174b66ebde`
+      `${API.URL}?q=${query}&exclude=daily,minutely,alerts&units=metric&appid=${API.KEY}`
     );
 
     return data;
